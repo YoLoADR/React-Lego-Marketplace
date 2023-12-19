e053c2f922c3d8dd913cfd6ca77f1ee63744fdb5
Popspow <154344440+Popspow@users.noreply.github.com>
2023-12-19 15:20:51 +0000

    j'ai modifié le titre du menu2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 export default function App() {
   return (
     <main className="main-container">
-      <div className="menu">Menu</div>
-      <div className="header">Header</div>
+      <div className="menu">Menu1</div>
+      <div className="menu">Sous-Menu</div>
+      <div className="header">Header1</div>
+      <div className="header">Header33JKEHH</div>
+      <div className="content">Content</div>
       <div className="content">Content</div>
       <div className="footer">Footer</div>
     </main>
