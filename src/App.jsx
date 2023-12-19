72b4ebb96b41b7f375c47ad01c6e50a64d52510e
YohannRavino1 <14688797-YohannRavino1@users.noreply.replit.com>
2023-12-19 12:03:21 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+export default function App() {
+  return (
+    <main className="main-container">
+      <div className="menu">Menu</div>
+      <div className="header">Header</div>
+      <div className="content">Content</div>
+      <div className="footer">Footer</div>
+    </main>
+  );
+}
